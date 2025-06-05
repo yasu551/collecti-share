697a78c34e8340c202f5d905b9f09b3f184bfe74
Yasuhiro Sugawara <yasu@sonicgarden.jp>
2025-06-06 08:10:43 +0900

    rails generatorで余計なものが生成されないように

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,10 @@ module CollectiShare
     config.i18n.default_locale = :ja
     config.yjit = !Rails.env.local?
 
-    # Don't generate system test files.
-    config.generators.system_tests = nil
+    config.generators do |g|
+      g.helper false
+      g.assets false
+      g.test_framework false
+    end
   end
 end
